4e6b930764a6de1f02a1d72e4dc62a525a1e460d
KingLemurs <xxdjroundtablexx@gmail.com>
2025-04-17 20:32:36 -0700

    adjusted car behavior

--- Assets/Scripts/assignment1/SteeringBehavior.cs
+++ Assets/Scripts/assignment1/SteeringBehavior.cs
@@ -53,7 +53,10 @@ public class SteeringBehavior : MonoBehaviour
 
         float targetAngle = Vector3.SignedAngle(transform.forward, dist.normalized, Vector3.up);
 
-
+        if (Mathf.Abs(targetAngle - transform.eulerAngles.z) < 5f)
+        {
+            targetAngle *= 0.01f;
+        }
 
         kinematic.SetDesiredRotationalVelocity( targetAngle * Time.deltaTime * 1000);
         kinematic.SetDesiredSpeed(Mathf.Max(dist.sqrMagnitude * Time.deltaTime * 5, 3) );
